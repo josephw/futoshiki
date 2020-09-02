9244c2db0cbfc1afb054643aeaa4d4e443ae008b
Joseph Walton <joe@kafsemo.org>
2020-09-02 21:42:02 +1000

    Build out an SVG UI.

--- futoshiki-js/browser-build.sh
+++ futoshiki-js/browser-build.sh
@@ -2,7 +2,8 @@
 
 $(npm bin)/tsc -m es6 --outDir d src/*.ts --target es2019 --moduleResolution node
 
-$(npm bin)/rollup d/index.js --file bundle.js --format iife --output.name abc
+$(npm bin)/rollup d/index.js --file static/bundle.js --format iife --output.name abc
+
 
 # Serve with
-# python3 -m http.server
+# python3 -m http.server --directory static
